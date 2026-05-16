88f4bb26c0739b83e16e15c493ab5b111dffe3bf
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2026-05-16 18:34:26 -0300

    Criada e desenvolvida classe FuncionarioTdbBO

---
+++ src/main/java/br/com/fiap/bo/FuncionarioTdbBO.java
@@ -0,0 +1,17 @@
+package br.com.fiap.bo;
+
+import br.com.fiap.dao.FuncionarioTdbDao;
+import br.com.fiap.entities.FuncionarioTdb;
+
+import java.sql.SQLException;
+
+public class FuncionarioTdbBO {
+
+    FuncionarioTdbDao funcionarioTdbDao;
+
+    // Inserir
+    public void InserirBO(FuncionarioTdb funcionarioTdb) throws SQLException, ClassNotFoundException {
+        FuncionarioTdbDao funcionarioTdbDAO = new FuncionarioTdbDao();
+        funcionarioTdbDAO.inserir(funcionarioTdb);
+    }
+}
