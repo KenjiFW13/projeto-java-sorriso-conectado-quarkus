1525a974f15451f7fe158886c90a0c6685f38964
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2026-05-16 19:29:21 -0300

    Criada e classe BeneficiarioResource e implementado métodos de inserção e deletar em verbos HTTP (POST, DELETE)

---
+++ src/main/java/br/com/fiap/BeneficiarioResource.java
@@ -0,0 +1,37 @@
+package br.com.fiap;
+
+import br.com.fiap.bo.BeneficiarioBO;
+
+import br.com.fiap.entities.Beneficiario;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+import jakarta.ws.rs.ext.Provider;
+
+import java.sql.SQLException;
+
+@Provider
+
+@Path("/beneficiario")
+public class BeneficiarioResource {
+
+    private BeneficiarioBO beneficiarioBO = new BeneficiarioBO();
+
+    // Inserir
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response inserirRs(Beneficiario beneficiario, @Context UriInfo uriInfo) throws SQLException, ClassNotFoundException {
+        beneficiarioBO.inserirBO(beneficiario);
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+        builder.path(Integer.toString(beneficiario.getIdBeneficiario()));
+        return Response.created(builder.build()).build();
+    }
+
+    // Deletar
+    @DELETE
+    @Path("{codigo}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response deletarRs(@PathParam("codigo") int codigo) throws SQLException, ClassNotFoundException {
+        beneficiarioBO.deletarBO(codigo);
+        return Response.ok().build();
+    }
+}
