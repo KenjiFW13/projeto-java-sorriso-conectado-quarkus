ff664e7f6fed79707aa3c13c95b770b588681b2d
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2026-05-17 14:05:20 -0300

    Adicionado outros métodos BO para a classe VoluntarioBO

--- src/main/java/br/com/fiap/bo/VoluntarioBO.java
+++ src/main/java/br/com/fiap/bo/VoluntarioBO.java
@@ -1,9 +1,11 @@
 package br.com.fiap.bo;
 
 import br.com.fiap.dao.VoluntarioDao;
+import br.com.fiap.entities.Beneficiario;
 import br.com.fiap.entities.Voluntario;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class VoluntarioBO {
 
@@ -13,5 +15,29 @@ public class VoluntarioBO {
     public void inserirBO(Voluntario voluntario) throws SQLException, ClassNotFoundException {
         VoluntarioDao voluntarioDAO = new VoluntarioDao();
         voluntarioDAO.inserir(voluntario);
+    }
+
+    // Deletar
+    public void deletarBO(int codigo) throws SQLException, ClassNotFoundException {
+        VoluntarioDao voluntarioDAO = new VoluntarioDao();
+        voluntarioDao.deletar(codigo);
+    }
+
+    // Update
+    public void atualizarBO(Voluntario voluntario) throws SQLException, ClassNotFoundException {
+        VoluntarioDao voluntarioDAO = new VoluntarioDao();
+        voluntarioDao.atualizar(voluntario);
+    }
+
+    // Select
+    public ArrayList<Voluntario> selecionarBO() throws SQLException, ClassNotFoundException {
+        voluntarioDao = new VoluntarioDao();
+        return (ArrayList<Voluntario>) voluntarioDao.selecionar();
+    }
+
+    // Select / Código
+    public Voluntario selecionarPorCodigoBo(int codigo) throws SQLException, ClassNotFoundException {
+        VoluntarioDao voluntarioDAO = new VoluntarioDao();
+        return voluntarioDao.selecionarPorCodigo(codigo);
     }
 }
