d091b906a7d6f3252794368bc9aeb0e09721f255
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2026-05-17 11:41:01 -0300

    Criado classe FuncionarioTdbResource e implementado método inserirRs (POST)

---
+++ src/main/java/br/com/fiap/FuncionarioTdbResource.java
@@ -0,0 +1,29 @@
+package br.com.fiap;
+
+import br.com.fiap.bo.FuncionarioTdbBO;
+import br.com.fiap.entities.FuncionarioTdb;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.*;
+import jakarta.ws.rs.ext.Provider;
+
+import java.sql.SQLException;
+
+@Provider
+
+@Path("/funcionariotdb")
+public class FuncionarioTdbResource {
+
+    private FuncionarioTdbBO funcionarioTdbBO = new FuncionarioTdbBO();
+
+    // Inserir
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response inserirRs(FuncionarioTdb funcionarioTdb, @Context UriInfo uriInfo) throws SQLException, ClassNotFoundException {
+        funcionarioTdbBO.inserirBO(funcionarioTdb);
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+        builder.path(Integer.toString(funcionarioTdb.getIdFuncionarioTdb()));
+        return Response.created(builder.build()).build();
+    }
+}
