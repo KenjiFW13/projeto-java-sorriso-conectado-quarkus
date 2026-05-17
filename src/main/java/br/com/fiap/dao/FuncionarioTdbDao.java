7c8c557f386bc6f4d547716fc66410ef3b35a2cd
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2026-05-17 13:30:36 -0300

    Adicionado outros métodos Dao para a classe FuncionarioTdbDao

--- src/main/java/br/com/fiap/dao/FuncionarioTdbDao.java
+++ src/main/java/br/com/fiap/dao/FuncionarioTdbDao.java
@@ -3,10 +3,8 @@ package br.com.fiap.dao;
 import br.com.fiap.conexoes.ConexaoFactory;
 import br.com.fiap.entities.FuncionarioTdb;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class FuncionarioTdbDao {
 
@@ -16,6 +14,7 @@ public class FuncionarioTdbDao {
         this.minhaConexao = new ConexaoFactory().conexao();
     }
 
+    // Inserir
     public String inserir(FuncionarioTdb funcionarioTdb) throws SQLException {
         PreparedStatement stmt = minhaConexao.prepareStatement("Insert Into T_TDB_FUNCIONARIOTDB values (?,?,?,?,?,?,?,?)");
         stmt.setInt(1, funcionarioTdb.getIdFuncionarioTdb());
@@ -31,6 +30,83 @@ public class FuncionarioTdbDao {
         stmt.close();
 
         return "Funcionário Cadastrado com Sucesso!!!";
+    }
+
+    // Deletar
+    public String deletar(int codigo) throws SQLException {
+        PreparedStatement stmt = minhaConexao.prepareStatement("Delete From T_TDB_FUNCIONARIOTDB where ID_FUNCIONARIOTDB =?");
+        stmt.setInt(1, codigo);
+
+        stmt.execute();
+        stmt.close();
+
+        return "Funcionário Deletado com Sucesso!!!";
+    }
+
+    // UpDate
+    public String atualizar(FuncionarioTdb funcionarioTdb) throws SQLException {
+        PreparedStatement stmt = minhaConexao.prepareStatement
+                ("Update T_TDB_FUNCIONARIOTDB set NOME_FUNCIONARIOTDB =?, CPF_FUNCIONARIOTDB =?, NASC_FUNCIONARIOTDB =?, FONE_FUNCIONARIOTDB =?, EMAIL_FUNCIONARIOTDB =?, END_FUNCIONARIOTDB =?, CAR_FUNCIONARIOTDB =? where ID_FUNCIONARIOTDB =?");
+        stmt.setString(1, funcionarioTdb.getNome());
+        stmt.setString(2, funcionarioTdb.getCpf());
+        stmt.setDate(3, Date.valueOf(funcionarioTdb.getDataNasc()));
+        stmt.setString(4, funcionarioTdb.getTelefone());
+        stmt.setString(5, funcionarioTdb.getEmail());
+        stmt.setString(6, funcionarioTdb.getEndereco());
+        stmt.setString(7, funcionarioTdb.getCargo());
+        stmt.setInt(8, funcionarioTdb.getIdFuncionarioTdb());
+
+        stmt.executeUpdate();
+        stmt.close();
+
+        return "Funcionário Atualizado com Sucesso!!!";
+    }
+
+    // Select
+    public ArrayList<FuncionarioTdb> selecionar() throws SQLException {
+        ArrayList<FuncionarioTdb> listaFuncionario = new ArrayList<FuncionarioTdb>();
+        PreparedStatement stmt = minhaConexao.prepareStatement("select * from T_TDB_FUNCIONARIOTDB");
+
+        ResultSet rs = stmt.executeQuery();
+        while(rs.next()){
+            FuncionarioTdb objFuncionario = new FuncionarioTdb();
+            objFuncionario.setIdFuncionarioTdb(rs.getInt(1));
+            objFuncionario.setNome(rs.getString(2));
+            objFuncionario.setCpf(rs.getString(3));
+            objFuncionario.setDataNasc(rs.getDate(4).toLocalDate());
+            objFuncionario.setTelefone(rs.getString(5));
+            objFuncionario.setEmail(rs.getString(6));
+            objFuncionario.setEndereco(rs.getString(7));
+            objFuncionario.setCargo(rs.getString(8));
+
+            listaFuncionario.add(objFuncionario);
+        }
+        return listaFuncionario;
+    }
+
+    // Select / Código
+    public FuncionarioTdb selecionarPorCodigo(int codigo) throws SQLException {
+        FuncionarioTdb objFuncionario = null;
+        PreparedStatement stmt = minhaConexao.prepareStatement("select * from T_TDB_FUNCIONARIOTDB where ID_FUNCIONARIOTDB = ?");
+        stmt.setInt(1, codigo);
+
+        ResultSet rs = stmt.executeQuery();
+        if (rs.next()) {
+            objFuncionario = new FuncionarioTdb();
+            objFuncionario.setIdFuncionarioTdb(rs.getInt(1));
+            objFuncionario.setNome(rs.getString(2));
+            objFuncionario.setCpf(rs.getString(3));
+            objFuncionario.setDataNasc(rs.getDate(4).toLocalDate());
+            objFuncionario.setTelefone(rs.getString(5));
+            objFuncionario.setEmail(rs.getString(6));
+            objFuncionario.setEndereco(rs.getString(7));
+            objFuncionario.setCargo(rs.getString(8));
+        }
+
+        rs.close();
+        stmt.close();
+
+        return objFuncionario;
     }
 }
 
