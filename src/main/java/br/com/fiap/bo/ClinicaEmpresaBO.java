8ce66f59b979de5860eca3f0711af32e259e19f4
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2026-05-17 13:57:08 -0300

    Adicionado outros métodos BO para a classe ClinicaEmpresaBO

--- src/main/java/br/com/fiap/bo/ClinicaEmpresaBO.java
+++ src/main/java/br/com/fiap/bo/ClinicaEmpresaBO.java
@@ -1,9 +1,11 @@
 package br.com.fiap.bo;
 
 import br.com.fiap.dao.ClinicaEmpresaDao;
+import br.com.fiap.entities.Beneficiario;
 import br.com.fiap.entities.ClinicaEmpresa;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ClinicaEmpresaBO {
 
@@ -13,5 +15,29 @@ public class ClinicaEmpresaBO {
     public void inserirBo(ClinicaEmpresa clinicaEmpresa) throws SQLException, ClassNotFoundException {
         ClinicaEmpresaDao clinicaEmpresaDAO = new ClinicaEmpresaDao();
         clinicaEmpresaDAO.inserir(clinicaEmpresa);
+    }
+
+    // Deletar
+    public void deletarBO(int codigo) throws SQLException, ClassNotFoundException {
+        ClinicaEmpresaDao clinicaEmpresaDAO = new ClinicaEmpresaDao();
+        clinicaEmpresaDao.deletar(codigo);
+    }
+
+    // Update
+    public void atualizarBO(ClinicaEmpresa clinicaEmpresa) throws SQLException, ClassNotFoundException {
+        ClinicaEmpresaDao clinicaEmpresaDAO = new ClinicaEmpresaDao();
+        clinicaEmpresaDao.atualizar(clinicaEmpresa);
+    }
+
+    // Select
+    public ArrayList<ClinicaEmpresa> selecionarBO() throws SQLException, ClassNotFoundException {
+        clinicaEmpresaDao = new ClinicaEmpresaDao();
+        return (ArrayList<ClinicaEmpresa>) clinicaEmpresaDao.selecionar();
+    }
+
+    // Select / Código
+    public ClinicaEmpresa selecionarPorCodigoBo(int codigo) throws SQLException, ClassNotFoundException {
+        ClinicaEmpresaDao clinicaEmpresaDAO = new ClinicaEmpresaDao();
+        return clinicaEmpresaDao.selecionarPorCodigo(codigo);
     }
 }
