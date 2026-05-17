ce1ddde8fb67e6cdeea15fc901cdcc2c20f37ea6
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2026-05-17 11:28:36 -0300

    Criado classe ClinicaEmpresaResource e implementado método inserirRs (POST)

---
+++ src/main/java/br/com/fiap/ClinicaEmpresaResource.java
@@ -0,0 +1,29 @@
+package br.com.fiap;
+
+import br.com.fiap.bo.ClinicaEmpresaBO;
+import br.com.fiap.entities.ClinicaEmpresa;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.*;
+import jakarta.ws.rs.ext.Provider;
+
+import java.sql.SQLException;
+
+@Provider
+
+@Path("ClinicaEmpresa")
+public class ClinicaEmpresaResource {
+
+    private ClinicaEmpresaBO clinicaEmpresaBO = new ClinicaEmpresaBO();
+
+    // Inserir
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response inserirRs(ClinicaEmpresa clinicaEmpresa, @Context UriInfo uriInfo) throws SQLException, ClassNotFoundException {
+        clinicaEmpresaBO.inserirBo(clinicaEmpresa);
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+        builder.path(Integer.toString(clinicaEmpresa.getIdEmpresa()));
+        return Response.created(builder.build()).build();
+    }
+}
